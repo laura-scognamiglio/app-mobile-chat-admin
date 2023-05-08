b317fef70f7c0ab36cb0332d242e73fe25e3b9a3
SarrD <leroidave@gmail.com>
2023-05-08 17:34:41 +0200

    feature update Room name OK

--- js/rooms.js
+++ js/rooms.js
@@ -1,22 +1,18 @@
 import { API , front } from "./constant.js";
 import Api from "./api.js"
-import { Token, refreshToken } from "./token.js";
+import { Token, refreshToken, id_admin } from "./token.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     
-    console.log("rooms");
-    const api = new Api();
-    // api.get({
-    //     route: '/users/signin',
-    //     success: data => { }
-    // })
+    //console.log("rooms.js");
+
     fetch(API + `/rooms/`, {
 		method: 'GET',
 		mode: 'cors',})
 
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            console.log("rooms : ",data);
 
             const itemsPerPage = 5;
 			let currentPage = 1;
@@ -33,45 +29,50 @@ document.addEventListener("DOMContentLoaded", () => {
 					const div = document.createElement('div');
 					div.setAttribute('class', 'item-class');
 					div.innerHTML += ` <label for="update-roomName" class="form-label">Room Name</label>`
-					div.innerHTML += `<input class="update-roomName" type="text" id="update-roomName" value='${item.name}' />`;
-					div.innerHTML += `<button type="submit" class="update-button-roomName btn btn-primary mb-3" id="update-button-roomName" value="${item.id}">Update room name</button>`;
+					div.innerHTML += `<input class="update-roomName" type="text" id="update-roomName-${item.id}" value='${item.name}' />`;
+					div.innerHTML += `<button type="submit" class="update-button-roomName btn btn-primary mb-3" id="update-button-roomName-${item.id}" value="${item.id}">Update room name</button>`;
 					container.appendChild(div);
+                })
 
-                    var inputUpdate = document.getElementsByClassName("update-button-roomName")
-					for (let i = 0; i < inputUpdate.length; i++){
-						//Boucle les boutons avec l'id de l'utilisateurs en value
-						const element = inputUpdate[i];
+				//boucle sur les elements "button" pour leur ajouter un eventListener "click"
+				var inputUpdate = document.getElementsByClassName("update-button-roomName")
+				for (let i = 0; i < inputUpdate.length; i++){
 
-                        element.addEventListener("click", () => {
-							//Event update login users
-							const UpdateRoomName = document.getElementById("update-roomName").value
-							const idRoom = element.value
+					const element = inputUpdate[i];
 
-							const data = {
-								name: UpdateRoomName,
-                            };
-                            
-                            fetch(API + `/admin/rooms/${idRoom}/update`, {
+					element.addEventListener("click", () => {
 
-                                method: 'PATCH',
-                                mode: 'cors',
-                                body: JSON.stringify(data),
-                                headers: {
-                                    'token': Token.get(),
-                                    'refreshToken': refreshToken.get(),
-                                    'Content-Type': 'application/json',
-                                    'Access-Control-Allow-Origin': '*',
-                                    'Access-Control-Allow-Credentials': 'true'
-                                },
-                            })
-                            .then(response => response.json())
-                            .then(data => console.log(data))
-                            .catch(error => console.error(error));
+						const idRoom = element.value
+						const updateRoomName = document.getElementById("update-roomName-"+idRoom).value
 
-                        })
-                        
-                    }
-                })     
+						const data = JSON.stringify({
+							name: updateRoomName,
+							id_role_admin: id_admin.get()
+						});
+
+						const token1Value = Token.get();
+						const refreshTokenValue = refreshToken.get()
+
+						fetch(API + `/admin/rooms/${idRoom}/update`, {
+
+							method: 'PATCH',
+							mode: 'cors',
+							body: data,
+							headers: {
+								'token1': token1Value,
+								'refreshToken': refreshTokenValue,
+								'Content-Type': 'application/json',
+								'Access-Control-Allow-Origin': '*',
+								'Access-Control-Allow-Credentials': 'true'
+							},
+						})
+						.then(response => response.json())
+						.then(data => alert(data.message))
+						.catch(error => console.error(error));
+
+					})
+
+				}
             }
             //pagination
             paginationData(currentPage);
